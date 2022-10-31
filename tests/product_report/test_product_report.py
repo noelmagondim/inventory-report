fbe3f6301bf3370cdd7f80f70ecb8885909e98e4
noelmagondim <noelmagondim@gmail.com>
2022-10-31 11:03:00 -0300

    Req 02: concluded

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        10,
+        "Titanium Dioxide",
+        "Target Corporation",
+        "2020-12-08",
+        "2023-12-08",
+        "FR29 5791 5333 58XR G4PR IG28 D08",
+        "local seco",
+    )
+
+    assert (
+        str(product) == (
+            f"O produto {product.nome_do_produto}"
+            f" fabricado em {product.data_de_fabricacao}"
+            f" por Target Corporation com validade"
+            f" até {product.data_de_validade}"
+            f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+        )
+    )
