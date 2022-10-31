5cfa3f771ca97d42efba7dd76706aa32103c2306
noelmagondim <noelmagondim@gmail.com>
2022-10-31 10:44:19 -0300

    Req 01: concluded

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        10,
+        "Titanium Dioxide",
+        "Target Corporation",
+        "2020-12-08",
+        "2023-12-08",
+        "FR29 5791 5333 58XR G4PR IG28 D08",
+        "instrucao 10",
+    )
+    assert product.id == 10
+    assert product.nome_do_produto == "Titanium Dioxide"
+    assert product.nome_da_empresa == "Target Corporation"
+    assert product.data_de_fabricacao == "2020-12-08"
+    assert product.data_de_validade == "2023-12-08"
+    assert product.numero_de_serie == "FR29 5791 5333 58XR G4PR IG28 D08"
+    assert product.instrucoes_de_armazenamento == "instrucao 10"
